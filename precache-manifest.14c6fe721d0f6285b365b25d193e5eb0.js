c0de7e358c6f4de56ac509e4f32c5de4dc4e06b3
Elisa <elisat15@uw.edu>
2020-07-19 13:42:27 -0700

    Updates

---
+++ precache-manifest.14c6fe721d0f6285b365b25d193e5eb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6dc8a807166407322d8152a3fdf0a4",
+    "url": "/stem-league-adventure/index.html"
+  },
+  {
+    "revision": "654e60c363f07599a058",
+    "url": "/stem-league-adventure/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6611561117f86e881aed",
+    "url": "/stem-league-adventure/static/css/main.f199d727.chunk.css"
+  },
+  {
+    "revision": "654e60c363f07599a058",
+    "url": "/stem-league-adventure/static/js/2.44e2184f.chunk.js"
+  },
+  {
+    "revision": "689e755408df23a6cba0012d32f8c68a",
+    "url": "/stem-league-adventure/static/js/2.44e2184f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6611561117f86e881aed",
+    "url": "/stem-league-adventure/static/js/main.171b9985.chunk.js"
+  },
+  {
+    "revision": "2dc3e9271c0faed9c0c1",
+    "url": "/stem-league-adventure/static/js/runtime-main.f025104a.js"
+  },
+  {
+    "revision": "7f92bd67f331b56d875fc7653447650b",
+    "url": "/stem-league-adventure/static/media/loading.7f92bd67.gif"
+  }
+]);
